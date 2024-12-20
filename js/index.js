afe27a0fcc3098ff202d3cb3a91c27c4125737ff
gerod <gerodann@gmail.com>
2024-12-20 17:31:19 +0300

    Функциональный конспект

--- js/index.js
+++ js/index.js
@@ -1,4 +1,46 @@
 "use strict";
 
-alert("Здравствуй, ученик!")
+const geoBtn = document.getElementById('geoBtn')
+const bioBtn = document.getElementById('bioBtn')
+const objBtn = document.getElementById('objBtn')
+const geo = document.getElementById('geo')
+const bio = document.getElementById('bio')
+const obj = document.getElementById('obj')
 
+
+let geoHidden = false;
+let bioHidden = false;
+let objHidden = false;
+
+geoBtn.addEventListener('click', function () {
+    if(geoHidden){
+        geo.setAttribute('class', 'main');
+        geoHidden = false;
+    }
+    else {
+        geo.setAttribute('class', 'hidden');
+        geoHidden = true;
+    }
+})
+
+bioBtn.addEventListener('click', function () {
+    if(bioHidden){
+        bio.setAttribute('class', 'main');
+        bioHidden = false;
+    }
+    else {
+        bio.setAttribute('class', 'hidden');
+        bioHidden = true;
+    }
+})
+
+objBtn.addEventListener('click', function () {
+    if(objHidden){
+        obj.setAttribute('class', 'main');
+        objHidden = false;
+    }
+    else {
+        obj.setAttribute('class', 'hidden');
+        objHidden = true;
+    }
+})

e6c5e74bc7c6bea4c1b732e2ac6ef2aa7cfb18b9
gerod <gerodann@gmail.com>
2025-01-21 11:30:34 +0300

    Изменены функции открытия окон

--- js/index.js
+++ js/index.js
@@ -12,34 +12,42 @@ let geoHidden = geo.getAttribute('class') === 'hidden';
 let bioHidden = bio.getAttribute('class') === 'hidden';
 let objHidden = obj.getAttribute('class') === 'hidden';
 
+
+function any(arr) {
+    let res = false;
+    for (let i=0; i < arr.length; i++) {
+        res ||= !arr[i];
+    }
+    return Boolean(res);
+}
+
 geoBtn.addEventListener('click', function () {
-    if(geoHidden){
+    if (geoHidden) {
         geo.setAttribute('class', 'main');
         geoHidden = false;
-    }
-    else {
+    } else if (any([bioHidden, objHidden])) {
         geo.setAttribute('class', 'hidden');
         geoHidden = true;
     }
+    console.log(bioHidden, objHidden);
 })
 
 bioBtn.addEventListener('click', function () {
-    if(bioHidden){
+    if (bioHidden) {
         bio.setAttribute('class', 'main');
         bioHidden = false;
-    }
-    else {
+    } else if (any([geoHidden, objHidden])) {
         bio.setAttribute('class', 'hidden');
         bioHidden = true;
     }
+    console.log([geoHidden, objHidden]);
 })
 
 objBtn.addEventListener('click', function () {
-    if(objHidden){
+    if (objHidden) {
         obj.setAttribute('class', 'main');
         objHidden = false;
-    }
-    else {
+    } else if (any([bioHidden, geoHidden])) {
         obj.setAttribute('class', 'hidden');
         objHidden = true;
     }
